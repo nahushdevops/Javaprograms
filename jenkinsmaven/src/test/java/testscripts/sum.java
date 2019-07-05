a432e8b1030f5921d3a71e72009dd152dc37098b
nahushdevops <nahushdevops@gmail.com>
2019-07-05 18:28:46 +0530

    New class added

---
+++ jenkinsmaven/src/test/java/testscripts/sum.java
@@ -0,0 +1,17 @@
+package testscripts;
+
+import org.testng.annotations.Test;
+
+public class sum {
+  @Test
+  public void sumofnumbers(){
+	  int x = 12;
+	  int y = 45;
+	  int z = x + y;
+	  
+	  System.out.println("Sum is = " + z);
+  }
+  
+  
+  
+}
